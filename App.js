0a034462906757a83311fb9060ba5a3581d54bbd
leonardo <desu987@hotmail.com>
2023-02-06 11:11:06 -0300

    added project

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { useState } from 'react'
+import { StyleSheet, Text, View, Button, TextInput, FlatList } from 'react-native';
+import { StatusBar} from 'expo-status-bar';
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoallInput';
 
 export default function App() {
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+  const [courseGoals, setCourseGoals] = useState([])
+
+  const startAddGoalHandler = () => {
+    setModalIsVisible(true);
+  }
+
+  const addGoalHandler = (enteredGoalText) => {
+    setCourseGoals((prevState) => [...prevState, {text: enteredGoalText, id: Math.random().toString()}]);
+    endAddGoalHandler();
+  }
+
+  const endAddGoalHandler = () => {
+    setModalIsVisible(false);
+  }
+
+  const deleteGoalHandler = (id) => {
+    setCourseGoals((prevState) => { return prevState.filter((goal) => goal.id !== id)});
+  }
+  //scrollview renderiza todos os itens, flatList não
+  /**
+   *    <ScrollView alwaysBounceVertical={false} style={styles.goalsContainer}>   
+        {courseGoals.map((goal, index) => <View style={styles.goalItem} key={index}>
+          <Text style={styles.goalText}>{goal}</Text>
+        </View>)}
+      </ScrollView>
+   */
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <>
+    <StatusBar style='light'/>
+    <View style={styles.appContainer}>
+      <Button title='Add New Goal' color="5e0acc" onPress={startAddGoalHandler}/>
+     <GoalInput onAddGoal={addGoalHandler} visible={modalIsVisible} onCancel={endAddGoalHandler}/>
+      <View style={styles.goalsContainer}>
+      <FlatList data={courseGoals} renderItem={itemData => {
+        return (<GoalItem id={itemData.item.id}  onDeleteItem={deleteGoalHandler} text={itemData.item.text}/>)}} alwaysBounceVertical={false} keyExtractor={(item, index) => {return item.id}} style={styles.goalsContainer}>   
+       
+      </FlatList>
+      </View>
+
     </View>
+    </>
+    
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop:50,
+    paddingHorizontal: 16,
+    backgroundColor: '#1e085a'
+  },
+  goalsContainer: {
+    flex: 5
   },
 });
